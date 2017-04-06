7f7ccf77ced313a4741cce1e3001a0c7b6db78cf
isit322 school <t.yee@bellevuecolllege.edu>
2017-04-06 16:48:12 -0700

    Committing Week01-ReactBasics

---
+++ Week01-ReactBasics/ReactBasics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class ReactBasics extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>An H1 element in a Isit322 Component</h1>
+                <p>More text</p>
+            </div>
+        )
+    }
+}
