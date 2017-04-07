96d78805116191c7b79c55e2d8c885162d87c480
isit322 school <t.yee@bellevuecolllege.edu>
2017-04-06 17:14:11 -0700

    Updated Week01-ReactBasics

---
+++ Week01-ReactBasics/ReactHello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class ReactHello extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>React Hello</h1>
+                <p>More text from the Hello page</p>
+            </div>
+        )
+    }
+}
