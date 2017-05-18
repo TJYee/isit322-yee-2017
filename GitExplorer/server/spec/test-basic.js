c5155afa259e37168b2dbbed8fa682a2d80313d1
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-18 15:29:29 -0700

    Added Jasmine support and initial tests to server side

--- GitExplorer/server/spec/test-basic.js
+++ GitExplorer/server/spec/test-basic.js
@@ -2,12 +2,26 @@
  * Created by charlie on 10/7/15.
  */
 
-describe('Elvenware Simple Plain Suite', function() {
+var request = require('supertest');
+var app = require('../app');
 
+describe('Elvenware Simple Plain Suite', function () {
     'use strict';
-
-    it('expects true to be true', function() {
+    
+    it('expects true to be true', function () {
         expect(true).toBe(true);
     });
-
+    
+    it('get the foo route', function (done) {
+        request(app)
+            .get('/gitapi/foo')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end(function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                done();
+            });
+    });
 });
