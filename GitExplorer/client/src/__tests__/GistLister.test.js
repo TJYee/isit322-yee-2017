73eb5639a7adb3e3f5d962a7520af09d7bdb2d6d
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-30 15:45:31 -0700

    Button test now passes

--- GitExplorer/client/src/__tests__/GistLister.test.js
+++ GitExplorer/client/src/__tests__/GistLister.test.js
@@ -30,17 +30,17 @@ describe('Gist Lister Suite', function () {
         />, div);
     });
     
-    /*it('renders and reads GistLister H2 text', () => {
+    it('renders and reads GistLister H2 text', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
             onChange={function () {
             }}
         />);
-        elfDebugEnzyme.display(wrapper);
+        //elfDebugEnzyme.display(wrapper.debug());
         const welcome = <h2>Gist Lister</h2>;
         expect(wrapper.containsMatchingElement(welcome)).toEqual(true);
-    });*/
+    });
     
     it('renders GistLister UL Element', () => {
         const wrapper = shallow(<GistLister
@@ -49,7 +49,25 @@ describe('Gist Lister Suite', function () {
             onChange={function () {
             }}
         />);
-        elfDebugEnzyme.getElement(wrapper, 'ul');
+        //elfDebugEnzyme.getElement(wrapper, 'ul');
         expect(wrapper.find('ul').length).toEqual(1);
+    });
+    
+    it('gistLister button responds to button click', () => {
+        //create variable to track button click status
+        let clicked = false;
+        //create function to assign as onClick
+        const callback = () => {
+            clicked = true;
+        };
+        
+        const wrapper = shallow(<GistLister
+            gitGistCanIterate={iterationBool}
+            gitGistList={bodyData}
+            onChange={callback}
+        />);
+        //elfDebugEnzyme.display(wrapper.debug());
+        wrapper.find('Button#gistLister').simulate('click');
+        expect(clicked).toEqual(true);
     });
 });
