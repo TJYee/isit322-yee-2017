29a067f8b5bb3030cb883a04205c4fb7a8685332
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 01:07:33 -0700

    Added introductory explanation of project

--- GitExplorer/client/src/components/Home.js
+++ GitExplorer/client/src/components/Home.js
@@ -6,6 +6,9 @@ class Home extends Component {
         return (
             <div>
                 <h2>Welcome to Git Explorer</h2>
+                <p>This project was made for Bellevue College's Spring 2017 ISIT 322 Course.</p>
+                <p>Please note that an environment variable of "GHTOKEN" needs to be defined as a GitHub Token with Gist
+                    permissions.</p>
             </div>
         )
     }
