2f53ed16407841bdfaad66f968cc0f514329d6e2
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 02:09:03 -0700

    Converted to ES6 syntax, now test for default and clicked data

--- GitExplorer/client/src/__tests__/GetFoo.test.js
+++ GitExplorer/client/src/__tests__/GetFoo.test.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import GetFoo from '../components/GetFoo';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import ElfDebugEnzyme from '../components/Debug/ElfDebugEnzyme';
 const elfDebugEnzyme = new ElfDebugEnzyme(false, 'GetFoo.test.js');
 
-describe('My GetFoo test suite', function() {
+describe('My GetFoo test suite', () => {
 
-    it('expects true to be true', function() {
+    it('expects true to be true', () => {
         expect(true).toBe(true);
+    });
+
+    it('renders default message for foo', () => {
+        const wrapper = shallow(<GetFoo />);
+        const sign = <p>state.foo: Waiting for server.</p>;
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
 
     it('renders button click message for foo', () => {
         const wrapper = shallow(<GetFoo />);
-        const nineSign = <p>state.foo: bar</p>;
+        const sign = <p>state.foo: bar</p>;
         wrapper.find('button#getFoo').simulate('click');
-        expect(wrapper.contains(nineSign)).toEqual(true);
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
+    });
+
+    it('renders default message for file', () => {
+        const wrapper = shallow(<GetFoo />);
+        const sign = <p>state.file: Get Foo File will be placed here.</p>;
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
+    });
+
+    it('renders button click message for file', () => {
+        const wrapper = shallow(<GetFoo />);
+        const sign = <p>state.file: getFoo.js</p>;
+        wrapper.find('button#getFoo').simulate('click');
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
 });
