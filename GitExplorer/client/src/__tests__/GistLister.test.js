428e668d457f9e788c132085dffec4323413a747
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 02:34:11 -0700

    Formatted to ES6 syntax

--- GitExplorer/client/src/__tests__/GistLister.test.js
+++ GitExplorer/client/src/__tests__/GistLister.test.js
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import GistLister from "../components/Git/Gist/GistLister";
-import {shallow} from "enzyme";
-import ElfDebugEnzyme from "../components/Debug/ElfDebugEnzyme";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GistLister from '../components/Git/Gist/GistLister';
+import {shallow} from 'enzyme';
+import ElfDebugEnzyme from '../components/Debug/ElfDebugEnzyme';
 const elfDebugEnzyme = new ElfDebugEnzyme(false, 'GistLister.test.js');
 
-describe('Gist Lister Suite', function () {
-    
+describe('Gist Lister Suite', () => {
     let bodyData = [{
         avatarUrl: '/',
         createdAt: 'Created At',
@@ -19,160 +18,160 @@ describe('Gist Lister Suite', function () {
         url: 'URL'
     }];
     let iterationBool = false;
-    
+
     it('renders the GistLister component without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />, div);
     });
-    
+
     it('renders and reads GistLister H2 text', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         elfDebugEnzyme.getElement(wrapper, 'h2');
         const welcome = <h2>Gist Lister</h2>;
         expect(wrapper.containsMatchingElement(welcome)).toEqual(true);
     });
-    
+
     it('renders GistLister ul Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         elfDebugEnzyme.getElement(wrapper, 'ul');
         expect(wrapper.find('ul').length).toEqual(1);
     });
-    
+
     it('renders GistLister default index li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Index: 0 / 0</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default createdAt li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Created At</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default description li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Description</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default gitPullUrl li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Git Pull URL</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default htmlUrl li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>HTML URL</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default id li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>ID</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default ownerLogin li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Owner Login</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default updatedAt li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>Updated At</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default url li Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <li>URL</li>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('renders GistLister default img Element', () => {
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
-            onChange={function () {
+            onChange={function() {
             }}
         />);
         const sign = <img src='/' alt='Avatar'/>;
         elfDebugEnzyme.getElement(wrapper, 'li');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
-    
+
     it('gistLister button responds to button click', () => {
         //create variable to track button click status
         let clicked = false;
@@ -180,7 +179,7 @@ describe('Gist Lister Suite', function () {
         const callback = () => {
             clicked = true;
         };
-        
+
         const wrapper = shallow(<GistLister
             gitGistCanIterate={iterationBool}
             gitGistList={bodyData}
