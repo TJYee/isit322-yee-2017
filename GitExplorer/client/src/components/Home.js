8afe23b73488bd1c10259fac3054e2b8c9d49d7b
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 16:39:05 -0700

    Updated more accurate description, content is now in a Jumbotron

--- GitExplorer/client/src/components/Home.js
+++ GitExplorer/client/src/components/Home.js
@@ -1,14 +1,17 @@
 import React, {Component} from "react";
+import {Jumbotron} from "react-bootstrap";
 import "../css/App.css";
 
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h2>Welcome to Git Explorer</h2>
-                <p>This project was made for Bellevue College's Spring 2017 ISIT 322 Course.</p>
-                <p>Please note that an environment variable of "GHTOKEN" needs to be defined as a GitHub Token with Gist
-                    permissions.</p>
+            <div className='col-sm-12'>
+                <Jumbotron>
+                    <h2>Welcome to Git Explorer</h2>
+                    <p>This project was made for Bellevue College's Spring 2017 ISIT 322 Course.</p>
+                    <p>Please note that an environment variable of "GHTOKEN" needs to be defined as a GitHub Token with
+                        Gist permissions in the "server" API.</p>
+                </Jumbotron>
             </div>
         )
     }
