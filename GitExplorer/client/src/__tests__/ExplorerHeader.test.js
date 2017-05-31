f703ee70714ed55e20aaa90cada0482b84ccf14e
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 02:31:20 -0700

    Formatted to ES6 syntax

--- GitExplorer/client/src/__tests__/ExplorerHeader.test.js
+++ GitExplorer/client/src/__tests__/ExplorerHeader.test.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ElfHeader from '../components/Header/ExplorerHeader';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import ElfDebugEnzyme from '../components/Debug/ElfDebugEnzyme';
 const elfDebugEnzyme = new ElfDebugEnzyme(false, 'ExplorerHeader.test.js');
 
-describe('My Header test suite', function() {
-
-    it('expects true to be true', function() {
+describe('My Header test suite', () => {
+    it('expects true to be true', () => {
         expect(true).toBe(true);
     });
 
-    it('renders and reads H2 text', () => {
+    it('renders and reads h2 text', () => {
         const wrapper = shallow(<ElfHeader />);
-        const welcome = <h2>Git Explorer</h2>;
+        const welcome = <h2 className='page-header'>Git Explorer</h2>;
         elfDebugEnzyme.getElement(wrapper, 'h2');
         expect(wrapper.contains(welcome)).toEqual(true);
     });
