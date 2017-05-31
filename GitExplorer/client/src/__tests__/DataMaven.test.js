7f016ba8c008dd68d23e8f9b4d0d47d3bd42f604
isit322 school <t.yee@bellevuecolllege.edu>
2017-05-31 03:20:15 -0700

    Tests for second p elemnt

--- GitExplorer/client/src/__tests__/DataMaven.test.js
+++ GitExplorer/client/src/__tests__/DataMaven.test.js
@@ -15,22 +15,30 @@ describe('My DataMaven test suite', () => {
         expect(true).toBe(true);
     });
 
-    it('renders and reads h2 header element', () => {
-        const wrapper = mount(<DataMaven />);
-        const sign = <h2>Welcome to Git Explorer</h2>;
-        elfDebugEnzyme.getFirst(wrapper, 'h2');
-        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
-    });
-
     it('renders the Navbar element', () => {
         const wrapper = mount(<DataMaven />);
         //elfDebugEnzyme.getFirst(wrapper, 'Navbar');
         expect(wrapper.find('Navbar').length).toEqual(1);
     });
 
-    it('renders and reads p home element', () => {
+    it('renders and reads h2 home element', () => {
+        const wrapper = mount(<DataMaven />);
+        const sign = <h2>Welcome to Git Explorer</h2>;
+        elfDebugEnzyme.getFirst(wrapper, 'h2');
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
+    });
+
+    it('renders and reads first p home element', () => {
         const wrapper = mount(<DataMaven />);
         const sign = <p>This project was made for Bellevue College's Spring 2017 ISIT 322 Course.</p>;
+        elfDebugEnzyme.getFirst(wrapper, 'p');
+        expect(wrapper.containsMatchingElement(sign)).toEqual(true);
+    });
+
+    it('renders and reads second p home element', () => {
+        const wrapper = mount(<DataMaven />);
+        const sign = <p>Please note that an environment variable of "GHTOKEN" needs to be defined as a GitHub Token with
+            Gist permissions.</p>;
         elfDebugEnzyme.getFirst(wrapper, 'p');
         expect(wrapper.containsMatchingElement(sign)).toEqual(true);
     });
