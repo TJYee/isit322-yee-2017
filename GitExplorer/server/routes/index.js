207828ad598ec2142a4f822e34ce77dcb4c42c8a
Taylor School Laptop <tayloryeebc@gmail.com>
2017-06-15 22:20:52 -0700

    Initial Micro Service foundation started, qux and its you-rang should be callable if run

--- GitExplorer/server/routes/index.js
+++ GitExplorer/server/routes/index.js
@@ -1,22 +1,55 @@
 let express = require('express');
 let router = express.Router();
+const requester = require('request');
 const ElfLogger = require('./elf-logger');
 const logger = new ElfLogger('index');
 
 router.use('/foo', require('./foo'));
 
+/* Micro Services */
+
+// gist Calls
+router.get('/gist', function (req, res, next) {
+    requester('http://localhost:30030').pipe(res);
+});
+
+// git-socket Calls
+router.get('/git-socket', function (req, res, next) {
+    requester('http://localhost:30031').pipe(res);
+});
+
+// git-user Calls
+router.get('/git-user', function (req, res, next) {
+    requester('http://localhost:30032').pipe(res);
+});
+
+// markdown Calls
+router.get('/markdown', function (req, res, next) {
+    requester('http://localhost:30033').pipe(res);
+});
+
+// qux Calls
+router.get('/qux', function (req, res, next) {
+    requester('http://localhost:30034').pipe(res);
+});
+
+router.get('/qux/you-rang', function (req, res, next) {
+    requester('http://localhost:30034/you-rang').pipe(res);
+});
+/* End of Micro Services */
+
 /* GET home page. */
 /*router.get('/', function(req, res, next) {
-    'use strict';
-    res.render('index', {title: 'server'});
-});*/
+ 'use strict';
+ res.render('index', {title: 'server'});
+ });*/
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
     'use strict';
     res.status(200).sendFile('index.html');
 });
 
-router.get('/:id', function(request, response) {
+router.get('/:id', function (request, response) {
     response.send({
         'result': 'success from 30026',
         'path': request.params.id
