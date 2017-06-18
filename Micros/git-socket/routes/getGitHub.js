6e3d85b259e37675a7deb67a40738ea427130eb2
Taylor School Laptop <tayloryeebc@gmail.com>
2017-06-18 15:32:00 -0700

    Added getGitHub script to gist and git-socket projects

---
+++ Micros/git-socket/routes/getGitHub.js
@@ -0,0 +1,16 @@
+let GitHub = require('github-api');
+let getGitHub = function () {
+    let gh;
+    // GitHub Token with Gist permissions
+    let gitToken = process.env.GHTOKEN;
+    if (gitToken !== undefined && gitToken !== '') {
+        gh = new GitHub({
+            token: gitToken
+        });
+    } else {
+        throw ('Need valid GHTOKEN definition! Please export GHTOKEN=[TOKEN HERE]');
+    }
+    return gh;
+};
+
+module.exports = getGitHub;
