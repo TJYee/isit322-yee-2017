6f1b0b5fbd00a693fa7e49007bfcbf8560e378fb
Taylor School Laptop <tayloryeebc@gmail.com>
2017-06-18 18:31:07 -0700

    Added you-rang functionality, Info panels need revision

--- GitExplorer/client/src/components/YouRang.js
+++ GitExplorer/client/src/components/YouRang.js
@@ -11,35 +11,17 @@ class YouRang extends Component {
         this.state = {
             message: 'Waiting for server.'
         };
-        logger.log('YouRang constructor called.')
+        logger.log('YouRang constructor called.');
     };
 
-    getFoo = () => {
+    fetchRang = (event) => {
         const that = this;
-        fetch('/foo')
-            .then(function (response) {
-                logger.log('GETONE-FETCH-ONE');
-                return response.json();
-            }).then(function (json) {
-            logger.log('GETONE-FETCH-TWO');
-            logger.log('parsed json', json);
-            that.setState(foo => (json));
-        }).catch(function (ex) {
-            logger.log('parsing failed', ex);
-        });
-    };
-
-    fetchUser = (event) => {
-        const that = this;
-        fetch('/git-user/user')
+        fetch('/' + event.target.id +'/you-rang')
             .then(function (response) {
                 return response.json();
             }).then(function (json) {
             logger.log('parsed json', json);
-            var gitUser = JSON.parse(json.body);
-            that.setState({
-                gitUser: gitUser
-            });
+            that.setState(data => (json));
         }).catch(function (ex) {
             // DISPLAY WITH LOGGER
             logger.log('parsing failed ', ex);
@@ -52,26 +34,50 @@ class YouRang extends Component {
             <div>
                 <div className='col-sm-8'>
                     <Jumbotron>
-                        <h2>Get Foo</h2>
+                        <h2>You Rang?</h2>
                         <Button
                             bsStyle='primary'
-                            id='getFoo'
-                            onClick={this.getFoo}
+                            id='gist'
+                            onClick={this.fetchRang}
                         >
-                            Get Foo
+                            Ring gist
+                        </Button>
+                        <Button
+                            bsStyle='primary'
+                            id='git-socket'
+                            onClick={this.fetchRang}
+                        >
+                            Ring git-socket
+                        </Button>
+                        <Button
+                            bsStyle='primary'
+                            id='git-user'
+                            onClick={this.fetchRang}
+                        >
+                            Ring git-user
+                        </Button>
+                        <Button
+                            bsStyle='primary'
+                            id='markdown'
+                            onClick={this.fetchRang}
+                        >
+                            Ring markdown
+                        </Button>
+                        <Button
+                            bsStyle='primary'
+                            id='qux'
+                            onClick={this.fetchRang}
+                        >
+                            Ring qux
                         </Button>
                         <p>
-                            state.foo: {this.state.foo}
-                        </p>
-                        <p>
-                            state.file: {this.state.file}
+                            message: {this.state.message}
                         </p>
                     </Jumbotron>
                 </div>
                 <div className='panel panel-info col-sm-4'>
                     <h2 className='panel-heading'>Info</h2>
                     <p>This component was designed to fetch simple data from an API server and save it to state.</p>
-                    <p>The page will then display the changed state.</p>
                 </div>
             </div>
         );
